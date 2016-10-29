d626f126d36cb4e7faed14cbd4a5a14120ea9b3f
gsamojlenko <clersaz@gmail.com>
2016-10-30 00:47:43 +0300

    add sum

--- calc.java
+++ calc.java
@@ -1,5 +1,10 @@
 public class calc{
 	public static void main (String[] args){
-	System.out.println("Git + Note!!!");
+		System.out.println("Git + Note!!!");
+		int first, second, result;
+		first = Integer.valueOf(args[0]);
+		second = Integer.valueOf(args[1]);
+		result = first + second;
+		System.out.println("Summ = " + result);
 	}
 }
