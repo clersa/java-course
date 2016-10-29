3fcb00a4f76f7dc5795ae58ba5236258a56cb52c
gsamojlenko <clersaz@gmail.com>
2016-10-30 00:53:25 +0300

    add if

--- calc.java
+++ calc.java
@@ -1,10 +1,16 @@
 public class calc{
 	public static void main (String[] args){
 		System.out.println("Git + Note!!!");
-		int first, second, result;
-		first = Integer.valueOf(args[0]);
-		second = Integer.valueOf(args[1]);
-		result = first + second;
-		System.out.println("Summ = " + result);
+		if (args.length == 0) {
+			System.out.println("No args!!!");
+		}
+		else
+		{
+			int first, second, result;
+			first = Integer.valueOf(args[0]);
+			second = Integer.valueOf(args[1]);
+			result = first + second;
+			System.out.println("Summ = " + result);
+		}
 	}
 }
