9ebc3fd135a46403e8a307359bb4bcd96770d1ca
gsamojlenko <clersaz@gmail.com>
2016-10-30 11:55:58 +0200

    add package w2

---
+++ ua/w2/Animal.java
@@ -0,0 +1,12 @@
+public class Animal implements Pet {
+	private final String name;
+
+	public Animal(final String name) {
+		this.name = name;
+	}
+
+	public void makeSound() {
+	System.out.println(
+		String.format("%s say : %s", this.name, "beep"));
+	}
+}
---
+++ ua/w2/Cat.java
@@ -0,0 +1,9 @@
+public class Cat extends Animal {
+
+	private Cat(final String name) {
+		super(name);
+	}
+
+	public void catchMouse() {
+	}
+}
